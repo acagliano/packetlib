4f43c51cc0ee6775592429fe9ecfe8afd8606592
Anthony Cagliano <acagliano97@gmail.com>
2021-08-27 11:31:05 -0400

    rewrite sending packet

--- src/main.c
+++ src/main.c
@@ -16,18 +16,12 @@ typedef struct {
     uint8_t id;
     bool (*init)(void);
     void (*process)(void);
-    bool (*read_to_size)(size_t size);
+    bool (*read_to_size)(size_t size, uint8_t* out);
     void (*write)(void *data, size_t size);
-} net_mode_t;
-net_mode_t srl_config;
+} srl_callbacks_t;
+srl_callbacks_t srl_funcs;
 
-typedef struct {
-	uint8_t srl_mode;
-	size_t srl_buf_size;
-	uint8_t packet_limit;
-} net_cfg_t;
-net_cfg_t net_config;
-size_t buf_seg_size;
+size_t srl_dbuf_size;
 size_t srl_bytes_read = 0;
 
 /* USB/SRL Subsystem */
@@ -106,12 +100,11 @@ enum _srl_modes {
 	SRL_MODE_SERIAL,
 	SRL_MODE_CEMU
 };
-bool pl_InitSubsystem(uint8_t *srl_buf, net_cfg_t *cfg, void* (*malloc)(size_t)){
-	if(srl_buf == NULL) return false;
+bool pl_InitSubsystem(uint8_t srl_mode, size_t srl_buf_size, void* (*malloc)(size_t)){
 	if((srl_mode>SRL_MODE_CEMU) || (srl_mode<SRL_MODE_SERIAL)) return false;
 	switch(srl_mode){
 		case SRL_MODE_SERIAL:
-			srl_config = {
+			srl_funcs = {
 				MODE_SERIAL,
 				init_usb,
 				usb_process,
@@ -121,7 +114,7 @@ bool pl_InitSubsystem(uint8_t *srl_buf, net_cfg_t *cfg, void* (*malloc)(size_t))
 			break;
 		
 		case SRL_MODE_CEMU:
-			srl_config = {
+			srl_funcs = {
 				MODE_CEMU_PIPE,
 				pipe_init,
 				NULL,
@@ -129,11 +122,12 @@ bool pl_InitSubsystem(uint8_t *srl_buf, net_cfg_t *cfg, void* (*malloc)(size_t))
 				cemu_send		// asm routine
 			};
 			break;
+		default:
+			return false;
 	}
-	srl_buf = malloc(cfg->srl_buf_size);
-	memcpy(&net_config, cfg, sizeof(net_cfg_t));
-	buf_seg_size = net_config.srl_buf_size>>1;
-	return srl_config.init();
+	if(!(srl_buf = malloc(srl_buf_size))) return false;
+	srl_dbuf_size = (srl_buf_size>>1);
+	return srl_funcs.init();
 }
 
 typedef struct _packet_segments {
@@ -146,16 +140,30 @@ size_t pl_PreparePacket(uint8_t ctl, ps_seg_t *ps, uint8_t arr_len, uint8_t *pac
 	for(uint8_t i=0; i<arr_len; i++){
 		uint8_t* addr = ps[i]->addr;
 		size_t len = ps[i]->len;
-		if((pos + len) > buf_seg_size) return pos;
+		//if((pos + len) > buf_seg_size) return pos;
 		memcpy(&packet[pos], addr, len);
 		pos += len;
 	}
 }
 
-size_t pl_SendPacket(const uint8_t *data, size_t len){
-	if(data==NULL) return 0;
-	if(len > buf_seg_size) return 0;
-	srl_config->write(data, len);
+
+#define MIN(x, y)	(x < y) ? x : y;
+bool pl_SendPacket(const uint8_t ctl, const uint8_t *data, size_t len){
+	bool multi = ((len + 1) > srl_dbuf_size);
+	if(data==NULL) return false;
+	if(len==0) return false;
+	if((len+1) < srl_dbuf_size){
+		srl_funcs->write(ctl, sizeof(uint8_t));
+		srl_funcs->write(data, len);
+		return true;
+	}
+	else {
+		for(size_t i = 0, uint8_t j=0; i < len; i+=(srl_dbuf_size-2), j++){
+			srl_funcs->write(ctl, sizeof(uint8_t));
+			srl_funcs->write(j, sizeof(uint8_t));
+			srl_funcs->write(data, MIN(srl_dbuf_size, len - i));
+		}
+	}	
 }
 
 size_t pl_ReadPacket(uint8_t *dest, size_t read_size, bool blocking){
